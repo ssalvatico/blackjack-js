766b6a1b0b69b6fe857eba233e3b6e1b4e20cd52
ssalvatico <ssalvatico@santafe.gov.ar>
2026-09-09 09:26:52 -0300

    correción del deployment
    correción del deployment

--- src/blackjack/usecases/modules/update-card-DOM.js
+++ src/blackjack/usecases/modules/update-card-DOM.js
@@ -9,6 +9,6 @@ export const agregarCartaDOM = (carta, turno, jugadoresCartas) => {
     const nuevaCarta    = document.createElement('img');
     nuevaCarta.id       = carta;
     nuevaCarta.classList.add('carta');
-    nuevaCarta.src      = `${import.meta.env.BASE_URL}assets/cartas/${carta}.png`;
+    nuevaCarta.src      = `/public/assets/cartas/${carta}.png`;
     jugadoresCartas[turno].append(nuevaCarta);
 };
